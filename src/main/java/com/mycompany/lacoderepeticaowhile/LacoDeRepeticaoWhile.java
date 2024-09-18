ca91f7a6bff390af5a07e789f094deb54ff88d1d
Collado66 <marcelocollado66@gmail.com>
2024-09-17 21:17:48 -0300

    PRIMEIRA VERSÃO

---
+++ src/main/java/com/mycompany/lacoderepeticaowhile/LacoDeRepeticaoWhile.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lacoderepeticaowhile;
+
+/**
+ *
+ * @author marce
+ */
+
+public class LacoDeRepeticaoWhile {
+    public static void main(String[] args) {
+        int i = 1; // Inicialização da variável
+
+        // Laço de repetição 'while' para imprimir números de 1 a 100
+        while (i <= 100) {
+            System.out.println("Número: " + i);
+            i++; // Incrementa o valor de 'i' a cada iteração
+        }
+    }
+}
